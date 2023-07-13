cca987a9727f292551e25b65dbbb861908154ad3
Kulllerkeks <57486459+Kulllerkeks@users.noreply.github.com>
2023-07-14 01:47:51 +0200

    initial checkin
    added a basic installscript to install programs after a new installation

---
+++ installtest.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd ~
+echo "beginning installation..."
+echo "-----------------------------------------------"
+echo "installing git and base-devel"
+sudo pacman -S --needed --noconfirm git base-devel
+echo "-----------------------------------------------"
+echo "loading programms from list:"
+programmlist=$(cat pacman-programm-list.txt) 
+programmlist=${programmlist//$'\n'}
+
+cat pacman-programm-list.txt
+echo "-----------------------------------------------"
+#pacman -S --noconfirm micro exa tldr fish mc code fzf gnome-shell-extensions alacritty kicad freecad discord teamspeak3 spotify-launcher inkscape btop
+# pacman --noconfirm | cat pacman-programm-list.txt
+
+# install all programms from the list
+sudo pacman -S --noconfirm - < pacman-programm-list.txt
+
+# install yay
+echo "-----------------------------------------------"
+echo "installing yay"
+cd ~
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg --syncdeps --install --needed --noconfirm
+echo "-----------------------------------------------"
+cd ~
